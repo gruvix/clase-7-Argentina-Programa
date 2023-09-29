e8c25ebd5adcb1e8208c44e34507f78ada5242f5
Gruvix <timmy.burton@hotmail.com>
2023-09-29 11:30:58 -0300

    Archivos iniciales (html, prueba, tarea (main))

---
+++ tarea-clase-7.js
@@ -0,0 +1,30 @@
+const $form = document.querySelector("#carta-a-santa");
+
+const nombre = $form.nombre.value;
+const ciudad = $form.ciudad.value;
+const comportamiento = $form.comportamiento.value;
+const descripcion = $form["descripcion-regalo"].value;
+
+
+console.log(nombre);
+console.log(ciudad);
+console.log(comportamiento);
+console.log(descripcion);
+
+function validarNombre(nombre) {
+  if (nombre.length === 0) {
+    return 'Este campo debe tener al menos 1 caracter';
+  }
+  if (nombre.length > 50) {
+    return 'Este campo debe tener menos de 50 caracteres';
+  }
+  return '';
+}
+
+/*
+* Hacer las funciones de validación de validarCiudad y validarDescripcionRegalo.
+* Escribir pruebas para esas funciones.
+*
+* Adicional: Escribir pruebas para las funciones de tareas anteriores.
+*
+* */
